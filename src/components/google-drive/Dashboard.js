5f7e5f90b67287f04ed763ebb0266ec3330820c2
kumar-sundaram-coder <sandyroshan2000@gmail.com>
2021-07-04 21:17:39 +0530

    Final Commit

--- src/components/google-drive/Dashboard.js
+++ src/components/google-drive/Dashboard.js
@@ -5,13 +5,17 @@ import AddFolderButton from "./AddFolderButton";
 import AddFileButton from "./AddFileButton";
 import Navbar from "./Navbar";
 import Folder from "./Folder";
+import File from "./File";
 import { useParams, useLocation } from "react-router";
 import FolderBreadcrumbs from "./FolderBreadcrumbs";
 
 export default function Dashboard() {
   const { folderId } = useParams();
   const { state = {} } = useLocation();
-  const { folder, childFolders } = useFolder(folderId, state.folder);
+  const { folder, childFolders, childFiles } = useFolder(
+    folderId,
+    state.folder
+  );
   console.log(childFolders);
 
   return (
@@ -32,6 +36,22 @@ export default function Dashboard() {
                 className="p-2"
               >
                 <Folder folder={childFolder} />
+              </div>
+            ))}
+          </div>
+        )}
+
+        {childFolders.length > 0 && childFiles.length > 0 && <hr />}
+
+        {childFiles && childFiles.length > 0 && (
+          <div className="d-flex flex-wrap">
+            {childFiles.map((childFile) => (
+              <div
+                key={childFile.id}
+                style={{ maxWidth: "250px" }}
+                className="p-2"
+              >
+                <File file={childFile} />
               </div>
             ))}
           </div>
